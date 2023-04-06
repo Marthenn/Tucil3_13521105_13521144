d20ab07a55e54d149ef766759cb1a36e4b8f26e3
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-04-06 20:05:02 +0700

    feat: newGraph constructor

--- src/lib/graph.go
+++ src/lib/graph.go
@@ -2,6 +2,7 @@ package graph
 
 import (
 	"container/list"
+	"strconv"
 )
 
 type Edge struct {
@@ -18,9 +19,47 @@ adj is the adjacency list
 type Graph struct {
 	nodes int
 	adj   []*list.List
+	names []string
 }
 
 func addEdge(g *Graph, src, dest int, weight float32) {
 	g.adj[src].PushBack(&Edge{dest, weight})
 	g.adj[dest].PushBack(&Edge{src, weight})
+}
+
+func newGraph(matrix [][]float32) *Graph {
+	g := &Graph{len(matrix), make([]*list.List, len(matrix)), make([]string, len(matrix))}
+
+	for i := 0; i < len(matrix); i++ {
+		g.adj[i] = list.New()
+		g.names[i] = strconv.Itoa(i)
+	}
+
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			if matrix[i][j] != 0 {
+				addEdge(g, i, j, matrix[i][j])
+			}
+		}
+	}
+
+	return g
+}
+
+func newGraphNamed(matrix [][]float32, names []string) *Graph {
+	g := &Graph{len(matrix), make([]*list.List, len(matrix)), names}
+
+	for i := 0; i < len(matrix); i++ {
+		g.adj[i] = list.New()
+	}
+
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			if matrix[i][j] != 0 {
+				addEdge(g, i, j, matrix[i][j])
+			}
+		}
+	}
+
+	return g
 }
