2949086b4d83814f1dc226a0299fe22576633d76
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-04-06 20:01:36 +0700

    feat: recreate Graph Struct and addEdge function

--- src/lib/graph.go
+++ src/lib/graph.go
@@ -1,8 +1,26 @@
 package graph
 
+import (
+	"container/list"
+)
+
+type Edge struct {
+	dest   int
+	weight float32
+}
+
+/*
+Graph is a struct that represents a graph
+Using an adjacency list
+nodes are represented by integers to save the number of nodes
+adj is the adjacency list
+*/
 type Graph struct {
 	nodes int
-	edges int
-	adj   [][]float32
-	names []string
+	adj   []*list.List
+}
+
+func addEdge(g *Graph, src, dest int, weight float32) {
+	g.adj[src].PushBack(&Edge{dest, weight})
+	g.adj[dest].PushBack(&Edge{src, weight})
 }
