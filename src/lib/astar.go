74c2bf15eda316b440aa548e62eeb4f72c706a53
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-04-07 23:43:33 +0700

    feat: astar

--- src/lib/astar.go
+++ src/lib/astar.go
@@ -1,9 +1,38 @@
 package lib
 
-func expandedWeight(dist []float32, euclid []float32) []float32 {
-	expanded := make([]float32, len(dist))
-	for i := 0; i < len(dist); i++ {
-		expanded[i] = dist[i] + euclid[i]
+func astar(g Graph, euclid []float32, start, end int) (path []int, dist float32) {
+	expanded := make([]float32, len(g.adj))
+	for i := 0; i < len(g.adj); i++ {
+		expanded[i] = euclid[i]
 	}
-	return expanded
+	visited := make([]bool, len(g.adj))
+	visited[start] = true
+	previous := make([]int, len(g.adj))
+	previous[start] = -1
+	dist = 0
+	for i := 0; i < len(g.adj); i++ {
+		min := -1
+		for j := 0; j < len(g.adj); j++ {
+			if !visited[j] && (min == -1 || expanded[j] < expanded[min]) {
+				min = j
+			}
+		}
+		if min == -1 {
+			break
+		}
+		visited[min] = true
+		for e := g.adj[min].Front(); e != nil; e = e.Next() {
+			edge := e.Value.(*Edge)
+			if !visited[edge.dest] && (expanded[min]+edge.weight < expanded[edge.dest]) {
+				expanded[edge.dest] = expanded[min] + edge.weight
+				previous[edge.dest] = min
+			}
+		}
+	}
+	path = make([]int, 0)
+	for i := end; i != -1; i = previous[i] {
+		path = append(path, i)
+	}
+	dist = expanded[end]
+	return
 }
